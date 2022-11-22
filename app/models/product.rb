3a0b2cb1add97731fe82827b51bf0976c29bf1a7
brice moulin <moulin_brice@hotmail.com>
2022-11-22 16:49:27 -0400

    Correctif validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,6 @@ class Product < ApplicationRecord
     has_many :basket_items
     has_one_attached :photo
     validates :name, presence: true
-    validates :price, presence: true
+    validates :pricing, presence: true
     validates :category, presence: true
 end
