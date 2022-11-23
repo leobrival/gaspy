9aebd29c516c7c2e7fc7382980055c1396386695
Léo Brival <leo.brival@gmail.com>
2022-11-23 09:57:05 -0400

    new baskets logic

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,5 +1,14 @@
 class Basket < ApplicationRecord
+  STATUSES = %w[ new pending finished ]
+  
   belongs_to :user
-  belongs_to :relay_point
+  belongs_to :relay_point, optional: true
   has_many :basket_items
+  has_many :products, through: :basket_items
+
+  validates :basket_status, inclusion: { in: STATUSES }
+
+  scope :all_finished,  -> { where(basket_status: "finished") }
+  scope :all_pending,   -> { where(basket_status: "pending") }
+  scope :all_new,       -> { where(basket_status: "new") }
 end
--- app/models/basket_item.rb
+++ app/models/basket_item.rb
@@ -1,4 +1,8 @@
 class BasketItem < ApplicationRecord
   belongs_to :product
   belongs_to :basket
+
+  def price
+    product.pricing * quantity
+  end
 end
