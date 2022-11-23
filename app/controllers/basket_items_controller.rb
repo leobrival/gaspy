ea5abf8fb699df8e74530624b2b6e37f456135ff
brice moulin <moulin_brice@hotmail.com>
2022-11-23 11:44:44 -0400

    Création du Controller Basket_items

--- app/controllers/basket_items_controller.rb
+++ app/controllers/basket_items_controller.rb
@@ -1,22 +1,32 @@
 class BasketItemsController < ApplicationController
-  def index
-  end
-
-  def new
-  end
+  before_action :set_basket_item, only: [:update, :destroy]
 
   def create
-  end
-
-  def show
-  end
-
-  def edit
+    @basket_item = BasketItem.new(basket_item_params)
+    @basket_item.basket = current_user.current_basket
+    @basket_item.product = Product.find(params[:product_id])
+    @basket_item.save
+    redirect_to products_path #vérifier la page de retour
   end
 
   def update
+    @basket_item.update(basket_item_params)
+    redirect_to products_path #vérifier la page de retour
   end
 
   def destroy
+    @basket_item.destroy
+    redirect_to products_path #vérifier la page de retour
   end
+
+  private
+
+  def basket_item_params
+    params.require(:basket_item).permit(:quantity)
+  end
+
+  def set_basket_item
+    @basket_item = BasketItem.find(params[:id])
+  end
+
 end
